e7f3595994f0f8070bb4c4e14439d9158df9fe1c
xuchuangyu <254568214@qq.com>
2022-09-06 09:25:02 +0800

    代码提交

--- app/config/config.js
+++ app/config/config.js
@@ -7,7 +7,7 @@ module.exports = {
         host:'127.0.0.1',
         port:3306,
         user:'root',
-        password:'123456',
+        password:'123qweASD!@#',
     },
     security:{
         secretKey:"abcdefg",
